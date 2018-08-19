2bc341b3c1443a019bd08e49897a9b4b607a1b74
thomson3914 <thomson3914@gmail.com>
2018-08-19 08:49:38 -0400

    initial upload

---
+++ models/burgers.js
@@ -0,0 +1,20 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    })
+  },
+
+  update: function (id, cb) {
+    orm.update("burgers", id, cb);
+  },
+
+  create: function(name, cb){
+    orm.create('burgers', name, cb);
+  }
+}
+
+module.exports = burger;
